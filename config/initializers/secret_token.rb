e31c62bb8e70cb0dd09bffffbc18555625eafb60
johnny-hsiao <johnnyhsiao87@gmail.com>
2016-03-28 12:45:14 +0000

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '498c68249bb1f5d0271b71d27c209b96b72c0e9670fa680e2ecb7fefb207b49336d8c5dfe99ab4445f6e23f71ed9538cb063aa06710a5dc2c7f33edb39f48cfc'
